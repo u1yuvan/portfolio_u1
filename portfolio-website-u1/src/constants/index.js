1309dd0b3a04cc1f42831c85cda828b0a3ac0aa2
u1yuvan <yuvanraj700@gmail.com>
2024-01-10 23:53:43 +0530

    implementing Modals section

--- portfolio-website-u1/src/constants/index.js
+++ portfolio-website-u1/src/constants/index.js
@@ -1,42 +1,43 @@
-import { 
-     facebook,  instagram, linkdin, Nike, GPT3, HooBank, HTML_CSS, react_JS} from "../assets"
+import {
+    facebook, instagram, linkdin, Nike, GPT3, HooBank, HTML_CSS, react_JS
+} from "../assets"
 
-export const AppText={
-    hello:'Hello,',
-    iam:'I am ',
-    yuvanraj:'Yuvan Raj',
-    desc:"Dedicated frontend developer skilled in HTML, CSS, and JavaScript, with a focus on React.js for building dynamic and efficient user interfaces. Proficient in leveraging Tailwind CSS for streamlined and responsive design.",
-    skills:'Skills & ',
-    experties:'Experties',
-    frontedDev:'Frontend Dev',
-    portfolio:'Portfolio',
-    creative:'My Creative ',
-    Contact:'Contact ',
-    us:'Us',
-    copywrite:'Copyright by Yuvanraj @2023'
+export const AppText = {
+    hello: 'Hello,',
+    iam: 'I am ',
+    yuvanraj: 'Yuvan Raj',
+    desc: "Dedicated frontend developer skilled in HTML, CSS, and JavaScript, with a focus on React.js for building dynamic and efficient user interfaces. Proficient in leveraging Tailwind CSS for streamlined and responsive design.",
+    skills: 'Skills & ',
+    experties: 'Experties',
+    frontedDev: 'Frontend Dev',
+    portfolio: 'Portfolio',
+    creative: 'My Creative ',
+    Contact: 'Contact ',
+    us: 'Us',
+    copywrite: 'Copyright by Yuvanraj @2023'
 
 }
 
 export const navLinks = [
     {
         id: "home",
-        title:"Home",
+        title: "Home",
     },
     {
         id: "aboutme",
-        title:"About Me",
+        title: "About Me",
     },
     {
         id: "skills",
-        title:"Skills",
+        title: "Skills",
     },
     {
         id: "portfolio",
-        title:"Portfolio",
+        title: "Portfolio",
     },
     {
         id: "contactme",
-        title:"Contact Me",
+        title: "Contact Me",
     }
 ];
 
@@ -45,105 +46,112 @@ export const navLinks = [
 
 
 
-export const portfolio =[
+export const portfolio = [
     {
-        id:1,
-        title:'Nike Website',
-        type:'react_js',
+        id: 1,
+        title: 'Nike Website',
+        type: 'react_js',
         desc:
-        "Crafted with React.js and stylized with Tailwind CSS, our front-end landing webpage channels the sophistication of Nike's design ethos. Dynamic and responsive, the interface seamlessly blends React's interactivity with Tailwind's sleek styling, delivering a user-centric experience reminiscent of top-tier brands like Nike.",
-        imageUrl:Nike,
+            "Crafted with React.js and stylized with Tailwind CSS, our front-end landing webpage channels the sophistication of Nike's design ethos. Dynamic and responsive, the interface seamlessly blends React's interactivity with Tailwind's sleek styling, delivering a user-centric experience reminiscent of top-tier brands like Nike.",
+        imageUrl: Nike,
     },
     {
-        id:3,
-        title:'GPT3 Website',
-        type:'react_js',
-        desc:"Transforming user experience into an intelligent journey, our React.js-powered front-end landing webpage mirrors the innovation of GPT-3. With dynamic content generation and responsive design",
-        imageUrl:GPT3,
+        id: 3,
+        title: 'GPT3 Website',
+        type: 'react_js',
+        desc: "Transforming user experience into an intelligent journey, our React.js-powered front-end landing webpage mirrors the innovation of GPT-3. With dynamic content generation and responsive design",
+        imageUrl: GPT3,
     },
     {
-        id:2,
-        title:'Hoo Bank',
-        type:'react_js',
-        desc:"Experience the future of online banking with our React.js-powered front-end landing page, inspired by modern bank apps. Tailwind CSS adds a touch of sleekness, ensuring a seamless and visually captivating interface that redefines the user experience in the world of finance.",
-        imageUrl:HooBank,
+        id: 2,
+        title: 'Hoo Bank',
+        type: 'react_js',
+        desc: "Experience the future of online banking with our React.js-powered front-end landing page, inspired by modern bank apps. Tailwind CSS adds a touch of sleekness, ensuring a seamless and visually captivating interface that redefines the user experience in the world of finance.",
+        imageUrl: HooBank,
     },
     {
-        id:4,
-        title:'HTML & CSS Landing webpage',
-        type:'learning_time',
-        desc:"Landing website using HTML & CSS During learning time ",
-        imageUrl:HTML_CSS,
+        id: 4,
+        title: 'HTML & CSS Landing webpage',
+        type: 'learning_time',
+        desc: "Landing website using HTML & CSS During learning time ",
+        imageUrl: HTML_CSS,
     },
     {
-        id:5,
-        title:'Grocery shopping site ',
-        type:'learning_time',
-        desc:"Grocery shopping site using React JS During learning time ",
-        imageUrl:react_JS,
+        id: 5,
+        title: 'Grocery shopping site ',
+        type: 'learning_time',
+        desc: "Grocery shopping site using React JS During learning time ",
+        imageUrl: react_JS,
     },
 
-    
+
 ]
 
-export const datas =[
+export const datas = [
     {
-        id:1,
-        title:'Nike Website',
-        type:'react_js',
+        id: 1,
+        title: 'Nike Website',
+        type: 'react_js',
         desc:
-        "Crafted with React.js and stylized with Tailwind CSS, our front-end landing webpage channels the sophistication of Nike's design ethos. Dynamic and responsive, the interface seamlessly blends React's interactivity with Tailwind's sleek styling, delivering a user-centric experience reminiscent of top-tier brands like Nike.",
-        imageUrl:Nike,
+
+            "Elevate your digital presence with a React.js and Tailwind CSS-powered front-end landing page, inspired by Nike's sleek design, seamlessly blending interactivity and aesthetics for a premium user experience.",
+        imageUrl: Nike,
+        technologies:["Javascript", "React", "Tailwind CSS"]
     },
     {
-        id:3,
-        title:'GPT3 Website',
-        type:'react_js',
-        desc:"Transforming user experience into an intelligent journey, our React.js-powered front-end landing webpage mirrors the innovation of GPT-3. With dynamic content generation and responsive design",
-        imageUrl:GPT3,
+        id: 3,
+        title: 'GPT3 Website',
+        type: 'react_js',
+        desc: 
+        "Redesigning the digital landscape with React.js, our front-end landing page mimics the responsive interface of GPT-3, delivering a dynamic and adaptive user experience.",
+        imageUrl: GPT3,
+        technologies:["Javascript", "React"]
     },
     {
-        id:2,
-        title:'Modern Bank website',
-        type:'react_js',
-        desc:"Experience the future of online banking with our React.js-powered front-end landing page, inspired by modern bank apps. Tailwind CSS adds a touch of sleekness, ensuring a seamless and visually captivating interface that redefines the user experience in the world of finance.",
-        imageUrl:HooBank,
+        id: 2,
+        title: 'Modern Bank website',
+        type: 'react_js',
+        desc: "Revolutionize your banking experience with a React.js and Tailwind CSS-driven front-end, combining modern aesthetics and functionality for a cutting-edge digital journey.",
+        imageUrl: HooBank,
+        technologies:["Javascript", "React", "Tailwind CSS"]
     },
     {
-        id:4,
-        title:'HTML & CSS Landing webpage',
-        type:'learning_time',
-        desc:"Landing website using HTML & CSS During learning time ",
-        imageUrl:HTML_CSS,
+        id: 4,
+        title: 'HTML & CSS Landing webpage',
+        type: 'learning_time',
+        desc: "Landing website using HTML & CSS During learning time ",
+        imageUrl: HTML_CSS,
+        technologies:["HTML", "CSS"]
     },
     {
-        id:5,
-        title:'Grocery shopping site ',
-        type:'learning_time',
-        desc:"Grocery shopping site using React JS During learning time ",
-        imageUrl:react_JS,
+        id: 5,
+        title: 'Grocery shopping site ',
+        type: 'learning_time',
+        desc: "Grocery shopping site using React JS During learning time ",
+        imageUrl: react_JS,
+        technologies:["Javascript", "React"]
     },
 
-    
+
 ]
 
 
 
-export const socialNetwork=[
+export const socialNetwork = [
     {
-        id:1,
-        logo:instagram,
-        url:'https://www.instagram.com/tubeguruji/'
+        id: 1,
+        logo: instagram,
+        url: 'https://www.instagram.com/tubeguruji/'
     },
-    
+
     {
-        id:1,
-        logo:linkdin,
-        url:'https://www.linkedin.com/in/rahul-sanap-652920108/'
+        id: 1,
+        logo: linkdin,
+        url: 'https://www.linkedin.com/in/rahul-sanap-652920108/'
     },
     {
-        id:1,
-        logo:facebook,
-        url:'https://www.facebook.com/rahul.sanap1'
+        id: 1,
+        logo: facebook,
+        url: 'https://www.facebook.com/rahul.sanap1'
     },
 ]
